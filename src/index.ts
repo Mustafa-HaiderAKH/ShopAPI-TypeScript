15b5fdfbaddfc042adb8411e392111af2ef8ec51
Your NameMustafa-Kok <Mustafa.andoo99@gmail.com>
2020-10-22 23:48:35 +0300

    add port heroku

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as express from "express";
 import { createConnection } from "typeorm";
 import { errRes } from "../helpers/tools";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 import v1 from "../route/app/v1";
 import * as ZC from "zaincash";
 import * as jwt from "jsonwebtoken";
